3227326b1450ee7eeb19195e5ff67207d3fbf19e
Daniel Jackson <email@danieljackson.co.uk>
2022-07-04 19:23:43 +0100

    Windows build

--- src/miniaudio.c
+++ src/miniaudio.c
@@ -1,5 +1,11 @@
+#ifdef __GNUC__ // __clang__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
+
 #define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio.h"
+
+#ifdef __GNUC__ // __clang__
 #pragma GCC diagnostic pop
+#endif
